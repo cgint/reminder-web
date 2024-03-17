7e7be91f545968d7e23785c073472dc051c11acd
cgint <christian.gintenreiter@gmail.com>
2024-03-17 23:14:12 +0100

    add comment as code does not work

--- src/Main.svelte
+++ src/Main.svelte
@@ -6,17 +6,38 @@
   
     onMount(() => {
         googleLoginInit()
+        //gapiLoaded()
+        // needs <script src="https://apis.google.com/js/api.js" async defer></script>
     });
+
+    // function gapiLoaded() {
+    //     gapi.load('client:auth2', function() {
+    //         gapi.client.init({
+    //             client_id: '144501535534-cc4hm6rceqoiur9oavrr5mihjkhm0leg.apps.googleusercontent.com',
+    //             scope: 'https://www.googleapis.com/auth/cloud-platform' // specify the necessary scope
+    //         }).then(() => {
+    //             console.log('gapi client initialized');
+    //         });
+    //     });
+    // }
+
+    // function fetchAccessToken() {
+    //     gapi.auth2.getAuthInstance().signIn().then(function(googleUser) {
+    //         let access_token = googleUser.getAuthResponse().access_token;
+    //         console.log('Access Token:', access_token);
+    //     });
+    // }
 
     function handleCredentialResponse(response) {
         let token = response.credential
         const payload = JSON.parse(atob(token.split('.')[1]));
         if (payload.email === "christian.gintenreiter@gmail.com") {
             isAuthenticated = true;
+            // fetchAccessToken()
         } else {
             isAuthenticated = false;
         }
-        isAuthenticated = true;
+        
     }
 
     function googleLoginInit() {
