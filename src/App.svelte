553703ce88295275beae471e7f2b8030cab74f44
cgint <christian.gintenreiter@gmail.com>
2024-03-24 08:05:42 +0100

    cache results seen in localstorage for today

--- src/App.svelte
+++ src/App.svelte
@@ -14,22 +14,22 @@
   let gpt_info_info = "";
   let gpt_info_analysis = "";
   let gpt_info_prev_input = "";
-  let gpt_info_processing = false;
+  let gpt_info_running = false;
   
   let info_key_value_pairs = {};
   let info_key_value_prev_input = "";
-  let info_key_value_processing = false;
+  let info_key_value_running = false;
   
   let linksInfoOrNews = "info";
   
   let news_links_list = [];
   let news_links_prev_input = "";
-  let news_links_processing = false;
+  let news_links_running = false;
   
   let gpt_news_analysis = "";
   let gpt_news_info = "";
   let gpt_news_analysis_prev_input = "";
-  let gpt_news_analysis_processing = false;
+  let gpt_news_analysis_running = false;
 
   let processingCount = 0;
   let processingValue = "";
@@ -39,18 +39,59 @@
   let processingTextIntervalTime = 500;
   let processingTextInterval = null;
 
-  let delete_todays_cache_processing = false;
+  let delete_todays_cache_running = false;
 
   function resetPrevInputValues() {
     info_key_value_prev_input = "";
     gpt_info_prev_input = "";
     gpt_news_analysis_prev_input = "";
     news_links_prev_input = "";
-    info_key_value_processing = false;
-    gpt_info_processing = false;
-    gpt_news_analysis_processing = false;
-    news_links_processing = false;
-    delete_todays_cache_processing = false;
+  }
+  function resetRunningStates() {
+    info_key_value_running = false;
+    gpt_info_running = false;
+    gpt_news_analysis_running = false;
+    news_links_running = false;
+    delete_todays_cache_running = false;
+  }
+
+  function result_cache_today_key_base() {
+    let today = new Date().toISOString().split('T')[0];
+    return `result_cache-${today}`;
+  }
+  function result_cache_today_key_for_stock(stock) {
+    return `${result_cache_today_key_base()}-${stock}`;
+  }
+  function result_cache_key_today(ticker, section) {
+    return `${result_cache_today_key_for_stock(ticker)}-${section}`;
+  }
+  function result_cache_set(ticker, section, value) {
+    localStorage.setItem(result_cache_key_today(ticker, section), value);
+  }
+  function result_cache_has_key(ticker, section) {
+    return localStorage.getItem(result_cache_key_today(ticker, section)) != null;
+  }
+  function result_cache_get(ticker, section) {
+    return localStorage.getItem(result_cache_key_today(ticker, section));
+  }
+  function result_cache_find_with_prefix(prefix) {
+      console.log("result_cache_find_with_prefix - prefix:", prefix);
+      let keys_found = [];
+      console.log("result_cache_find_with_prefix - keys in localstorage:", localStorage.length);
+      for (let i = 0; i < localStorage.length; i++) {
+        let key = localStorage.key(i);
+        if (key.startsWith(prefix)) {
+          keys_found.push(key);
+        }
+      }
+      return keys_found;
+    }
+
+  function result_cache_delete_today_stock(ticker) {
+    let today_key_stock = result_cache_today_key_for_stock(ticker);
+    let keys_to_remove = result_cache_find_with_prefix(today_key_stock);
+    console.log("keys_to_remove:", keys_to_remove);
+    keys_to_remove.forEach(key => localStorage.removeItem(key));
   }
 
   onMount(() => {
@@ -116,7 +157,7 @@
     startProcessing();
     try {
       if (getUpperCaseInputForAction()) {
-        await deleteTodaysCache()
+        await deleteTodaysCacheForStock()
       }
     } finally {
       stopProcessing();
@@ -159,118 +200,143 @@
     }
   }
   async function fetchInfoData() {
-    if (info_key_value_processing || userInput == info_key_value_prev_input) {
+    if (info_key_value_running || userInput == info_key_value_prev_input) {
       return;
     }
-    info_key_value_processing = true;
-    try {
-      if (userInput != info_key_value_prev_input) {
-        infos_ticker_name = "";
-        info_key_value_pairs = {'Loading.': 'Please wait...'};
-      }
-      await axios.get(
-        `${api_url}/data/${userInput}`, { headers: { 'password': password } }
-      ).then(response => {
-        console.log("response:", response);
-        info_key_value_prev_input = userInput;
-        info_key_value_pairs = response.data;
-        infos_ticker_name = ` for ${response.data['shortName']} (${userInput})`;
-      }).catch(error => {
+    if(!result_cache_has_key(userInput, "info_key_value_pairs")) {
+      info_key_value_running = true;
+      try {
+        if (userInput != info_key_value_prev_input) {
+          infos_ticker_name = "";
+          info_key_value_pairs = {'Loading.': 'Please wait...'};
+        }
+        await axios.get(
+          `${api_url}/data/${userInput}`, { headers: { 'password': password } }
+        ).then(response => {
+          console.log("response:", response);
+          info_key_value_prev_input = userInput;
+          result_cache_set(userInput, "info_key_value_pairs", JSON.stringify(response.data));
+          result_cache_set(userInput, "infos_ticker_name", ` for ${response.data['shortName']} (${userInput})`);
+        }).catch(error => {
+          console.error("Error fetching data:", error);
+          info_key_value_pairs = {'Error:': "Error fetching data: " + error};
+        }).finally(() => {
+          info_key_value_running = false;
+        });
+      } catch (error) {
+        info_key_value_running = false;
         console.error("Error fetching data:", error);
-        info_key_value_pairs = {'Error:': "Error fetching data: " + error};
-      }).finally(() => {
-        info_key_value_processing = false;
-      });
-    } catch (error) {
-      info_key_value_processing = false;
-      console.error("Error fetching data:", error);
-    } 
+      } 
+    }
+    if (result_cache_has_key(userInput, "info_key_value_pairs")) {
+      info_key_value_pairs = JSON.parse(result_cache_get(userInput, "info_key_value_pairs"));
+      infos_ticker_name = result_cache_get(userInput, "infos_ticker_name");
+    }
   }
   async function load_info_gpt_analysis() {
-    if (gpt_info_processing || userInput == gpt_info_prev_input) {
+    if (gpt_info_running || userInput == gpt_info_prev_input) {
       return;
     }
-    gpt_info_processing = true;
-    try {
-      if (userInput != gpt_info_prev_input) {
-        gpt_info_info = "";
-        gpt_info_analysis = 'Analysing. Please wait...';
+    if(!result_cache_has_key(userInput, "gpt_info_analysis")) {
+      gpt_info_running = true;
+      try {
+        if (userInput != gpt_info_prev_input) {
+          gpt_info_info = "";
+          gpt_info_analysis = 'Analysing. Please wait...';
+        }
+        await axios.get(
+          `${api_url}/gpt/analysis/${userInput}/yfinance`, { headers: { 'password': password } }
+        ).then(response => {
+          gpt_info_prev_input = userInput;
+          result_cache_set(userInput, "gpt_info_analysis", response.data['analysis']);
+          result_cache_set(userInput, "gpt_info_info", " ("+response.data['source']+" for "+response.data['ticker']+")");
+        }).catch(error => {
+          console.error("Error fetching data:", error);
+          gpt_info_analysis = "Error fetching data: " + error;
+        }).finally(() => {
+          gpt_info_running = false;
+        });
+      } catch (error) {
+        gpt_info_running = false;
+        console.error("Error fetching gpt info analysis:", error);
       }
-      await axios.get(
-        `${api_url}/gpt/analysis/${userInput}/yfinance`, { headers: { 'password': password } }
-      ).then(response => {
-        gpt_info_prev_input = userInput;
-        gpt_info_analysis = response.data['analysis'];
-        gpt_info_info = " ("+response.data['source']+" for "+response.data['ticker']+")";
-      }).catch(error => {
-        console.error("Error fetching data:", error);
-        gpt_info_analysis = "Error fetching data: " + error;
-      }).finally(() => {
-        gpt_info_processing = false;
-      });
-    } catch (error) {
-      gpt_info_processing = false;
-      console.error("Error fetching gpt info analysis:", error);
+    }
+    if (result_cache_has_key(userInput, "gpt_info_analysis")) {
+      gpt_info_analysis = result_cache_get(userInput, "gpt_info_analysis");
+      gpt_info_info = result_cache_get(userInput, "gpt_info_info");
     }
   }
   async function load_news_links() {
-    if (news_links_processing || userInput == news_links_prev_input) {
+    if (news_links_running || userInput == news_links_prev_input) {
       return;
     }
-    news_links_processing = true;
-    try {
-      if (userInput != news_links_prev_input) {
-        news_links_list = [];
+    if(!result_cache_has_key(userInput, "news_links_list")) {
+      news_links_running = true;
+      try {
+        if (userInput != news_links_prev_input) {
+          news_links_list = [];
+        }
+        await axios.get(
+          `${api_url}/data/${userInput}/news/overview`, { headers: { 'password': password } }
+        ).then(response => {
+          news_links_prev_input = userInput;
+          result_cache_set(userInput, "news_links_list", JSON.stringify(response.data['news']['data']));
+        }).catch(error => {
+          console.error("Error fetching data:", error);
+          news_links_list = [];
+        }).finally(() => {
+          news_links_running = false;
+        });
+      } catch (error) {
+        news_links_running = false;
+        console.error("Error fetching news links:", error);
       }
-      await axios.get(
-        `${api_url}/data/${userInput}/news/overview`, { headers: { 'password': password } }
-      ).then(response => {
-        news_links_prev_input = userInput;
-        news_links_list = response.data['news']['data'];
-      }).catch(error => {
-        console.error("Error fetching data:", error);
-        news_links_list = [];
-      }).finally(() => {
-        news_links_processing = false;
-      });
-    } catch (error) {
-      news_links_processing = false;
-      console.error("Error fetching news links:", error);
+    }
+    if (result_cache_has_key(userInput, "news_links_list")) {
+      news_links_list = JSON.parse(result_cache_get(userInput, "news_links_list"));
     }
   }
   async function load_news_gpt_analysis() {
-    if (gpt_news_analysis_processing || userInput == gpt_news_analysis_prev_input) {
+    if (gpt_news_analysis_running || userInput == gpt_news_analysis_prev_input) {
       return;
     }
-    gpt_news_analysis_processing = true;
-    try {
-      if (userInput != gpt_news_analysis_prev_input) {
-        gpt_news_info = "";
-        gpt_news_analysis = 'Analysing. Please wait...';
+    if(!result_cache_has_key(userInput, "gpt_news_analysis")) {
+      gpt_news_analysis_running = true;
+      try {
+        if (userInput != gpt_news_analysis_prev_input) {
+          gpt_news_info = "";
+          gpt_news_analysis = 'Analysing. Please wait...';
+        }
+        await axios.get(
+          `${api_url}/gpt/analysis/${userInput}/news`, { headers: { 'password': password } }
+        ).then(response => {
+          gpt_news_analysis_prev_input = userInput;
+          result_cache_set(userInput, "gpt_news_analysis", response.data['analysis']);
+          result_cache_set(userInput, "gpt_news_info", " ("+response.data['source']+" for "+response.data['ticker']+")");
+        }).catch(error => {
+          console.error("Error fetching data:", error);
+          gpt_news_analysis = "Error fetching data: " + error;
+        }).finally(() => {
+          gpt_news_analysis_running = false;
+        });
+      } catch (error) {
+        gpt_news_analysis_running = false;
+        console.error("Error fetching gpt news analysis:", error);
       }
-      await axios.get(
-        `${api_url}/gpt/analysis/${userInput}/news`, { headers: { 'password': password } }
-      ).then(response => {
-        gpt_news_analysis_prev_input = userInput;
-        gpt_news_analysis = response.data['analysis'];
-        gpt_news_info = " ("+response.data['source']+" for "+response.data['ticker']+")";
-      }).catch(error => {
-        console.error("Error fetching data:", error);
-        gpt_news_analysis = "Error fetching data: " + error;
-      }).finally(() => {
-        gpt_news_analysis_processing = false;
-      });
-    } catch (error) {
-      gpt_news_analysis_processing = false;
-      console.error("Error fetching gpt news analysis:", error);
+    }
+    if (result_cache_has_key(userInput, "gpt_news_analysis")) {
+      gpt_news_analysis = result_cache_get(userInput, "gpt_news_analysis");
+      gpt_news_info = result_cache_get(userInput, "gpt_news_info");
     }
   }
-  async function deleteTodaysCache() {
-    if (delete_todays_cache_processing) {
+  async function deleteTodaysCacheForStock() {
+    if (delete_todays_cache_running) {
       return;
     }
-    resetPrevInputValues()
-    delete_todays_cache_processing = true;
+    resetPrevInputValues();
+    resetRunningStates();
+    result_cache_delete_today_stock(userInput);
+    delete_todays_cache_running = true;
     try {
       await axios.delete(
         `${api_url}/cache/${userInput}/today`, { headers: { 'password': password } }
@@ -279,10 +345,10 @@
       }).catch(error => {
         console.error("Error deleting cache:", error);
       }).finally(() => {
-        delete_todays_cache_processing = false;
+        delete_todays_cache_running = false;
       });
     } catch (error) {
-      delete_todays_cache_processing = false;
+      delete_todays_cache_running = false;
       console.error("Error deleting cache:", error);
     }
   }
